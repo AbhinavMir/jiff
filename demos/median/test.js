327c75eedd21698410385e6874f05475c3f19829
Abhinav Srivastava <atg271@gmail.com>
2024-01-08 20:52:18 -0500

    wip

--- demos/median/test.js
+++ demos/median/test.js
@@ -1,147 +1,65 @@
-// Chai
-//var expect = require('chai').expect;
 var assert = require('chai').assert;
-
 var mpc = require('./mpc.js');
 
-// Generic Testing Parameters
+// Test parameters
 var party_count = 4;
-var parallelismDegree = 5; // Max number of test cases running in parallel
-var n = 15;
+var test_cases = 10; // Number of test cases
 
-// Parameters specific to this demo
-var maxElement = 13;
-var maxLength = 10;
-
-var showProgress = true;
-var Zp = 13;
-
-/**
- * CHANGE THIS: Generate inputs for your tests
- * Should return an object with this format:
- * {
- *   'party_id': [ 'test1_input', 'test2_input', ...]
- * }
- */
+// Function to generate test inputs
 function generateInputs(party_count) {
   var inputs = {};
   for (var i = 1; i <= party_count; i++) {
     inputs[i] = [];
-  }
-
-
-  for (var t = 0; t < n; t++) {
-    var length = Math.floor(Math.random() * maxLength) + 1;
-    for (var p = 1; p <= party_count; p++) {
-      var arr = [];
-      while (arr.length < length) {
-        arr.push(Math.floor(Math.random() * maxElement / party_count));
-      }
-
-      inputs[p][t] = arr;
+    for (var t = 0; t < test_cases; t++) {
+      // Generate arrays of random lengths with random numbers
+      var length = Math.floor(Math.random() * 10) + 1; // Random length from 1 to 10
+      var arr = Array.from({ length }, () => Math.floor(Math.random() * 100));
+      inputs[i].push(arr);
     }
   }
-
   return inputs;
 }
 
-/**
- * CHANGE THIS: Compute the expected results not in MPC
- * @param {object} inputs - same format as generateInputs output.
- * Should return a single array with the expected result for every test in order
- *   [ 'test1_output', 'test2_output', ... ]
- */
+// Function to manually compute expected medians
 function computeResults(inputs) {
   var results = [];
-
-  for (var t = 0; t < n; t++) {
-    var array = inputs[1][t].slice(); // Shallow copy, so that when modifying things are not changed!
-
+  for (var t = 0; t < test_cases; t++) {
+    // Sum arrays element-wise and sort
+    var sumArray = inputs[1][t].slice();
     for (var p = 2; p <= party_count; p++) {
-      var tmp = inputs[p][t];
-      for (var i = 0; i < array.length; i++) {
-        array[i] += tmp[i];
+      for (var i = 0; i < sumArray.length; i++) {
+        sumArray[i] += inputs[p][t][i];
       }
     }
+    sumArray.sort((a, b) => a - b);
 
-    array = array.sort(function (a, b) {
-      return a - b;
-    });
-    results.push(array);
+    // Compute median
+    var mid = Math.floor(sumArray.length / 2);
+    var median = sumArray.length % 2 !== 0 ? sumArray[mid] : (sumArray[mid - 1] + sumArray[mid]) / 2;
+    results.push(median);
   }
-
   return results;
 }
 
-/**
- * Do not change unless you have to.
- */
-// eslint-disable-next-line no-undef
-describe('Test', function () {
-  this.timeout(0); // Remove timeout
-
-  // eslint-disable-next-line no-undef
-  it('Exhaustive', function (done) {
-    var count = 0;
-
+describe('MPC Bubble Sort Median Test', function () {
+  it('should compute the correct median for each test case', function (done) {
+    this.timeout(0); // Disable timeout
     var inputs = generateInputs(party_count);
-    var realResults = computeResults(inputs);
+    var expectedResults = computeResults(inputs);
 
-    var onConnect = function (jiff_instance) {
-      var partyInputs = inputs[jiff_instance.id];
-
-      var testResults = [];
-      (function one_test_case(j) {
-        if (jiff_instance.id === 1 && showProgress) {
-          console.log('\tStart ', j > partyInputs.length ? partyInputs.length : j, '/', partyInputs.length);
-        }
-
-        if (j < partyInputs.length) {
-          var promises = [];
-          for (var t = 0; t < parallelismDegree && (j + t) < partyInputs.length; t++) {
-            promises.push(mpc.compute(partyInputs[j + t], jiff_instance));
+    // Connect to the server and run tests
+    mpc.connect('http://localhost:8080', 'test', { 
+      party_count: party_count,
+      onConnect: function (jiff_instance) {
+        var promises = inputs[jiff_instance.id].map(input => mpc.compute(input, jiff_instance));
+        Promise.all(promises).then(function (computedResults) {
+          for (var i = 0; i < computedResults.length; i++) {
+            assert.strictEqual(computedResults[i], expectedResults[i], `Test case ${i + 1} failed`);
           }
-
-          Promise.all(promises).then(function (parallelResults) {
-            for (var t = 0; t < parallelResults.length; t++) {
-              testResults.push(parallelResults[t]);
-            }
-
-            one_test_case(j+parallelismDegree);
-          });
-
-          return;
-        }
-
-        // If we reached here, it means we are done
-        count++;
-        for (var i = 0; i < testResults.length; i++) {
-          // construct debugging message
-          var ithInputs = inputs[1][i] + '';
-          for (var p = 2; p <= party_count; p++) {
-            ithInputs += ',' + inputs[p][i];
-          }
-          var msg = 'Party: ' + jiff_instance.id + '. inputs: [' + ithInputs + ']';
-
-          // assert results are accurate
-          try {
-            assert.deepEqual(testResults[i].toString(), realResults[i].toString(), msg);
-          } catch (assertionError) {
-            done(assertionError);
-            done = function () { };
-          }
-        }
-
-        jiff_instance.disconnect(true);
-        if (count === party_count) {
+          jiff_instance.disconnect();
           done();
-        }
-      })(0);
-    };
-
-    var options = { party_count: party_count, onError: console.log, onConnect: onConnect, Zp: Zp, crypto_provider: true};
-    for (var i = 0; i < party_count; i++) {
-      mpc.connect('http://localhost:8080', 'mocha-test', options);
-    }
+        });
+      }
+    });
   });
 });
